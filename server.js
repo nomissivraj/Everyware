085e2f454588a1aa81233488b6d9a6ba88420f23
Simon <simonpeterjarvis@gmail.com>
2018-10-31 22:04:26 +0000

    Added routes for basic front end testing

--- server.js
+++ server.js
@@ -1,24 +1,42 @@
 //MQTT Dependencies
-var mqtt = require('mqtt')
-var MQTT_TOPIC = "diaryentry"; //MQTT topic is test
-var MQTT_ADDR = "mqtt://broker.i-dat.org:80"; //Use the broker address here
-var MQTT_PORT = 80; //And broker's port
+const mqtt = require('mqtt')
+const MQTT_TOPIC = "diaryentry"; //MQTT topic is test
+const MQTT_ADDR = "mqtt://broker.i-dat.org:80"; //Use the broker address here
+const MQTT_PORT = 80; //And broker's port
+const WEB_PORT = 4000;
 
 //dependencies for general nodejs server routing etc...
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
 
-var personalityInsight = require('./personality-insight.js');
+const personalityInsight = require('./personality-insight.js');
 
-var client = mqtt.connect(MQTT_ADDR); //Create a new connection (use the MQTT adress)
+const client = mqtt.connect(MQTT_ADDR); //Create a new connection (use the MQTT adress)
 
-client.on('connect', function () { //connect the MQTT client
+client.on('connect', () => { //connect the MQTT client
     client.subscribe(MQTT_TOPIC, {
         qos: 2
     }); //Subscribe to the topic
 });
 
-client.on('message', function (MQTT_TOPIC, message) {
+client.on('message', (MQTT_TOPIC, message) => {
     personalityInsight.AnalysePersonality(message.toString()); //send json object recieved to personality-insight.js to analysis
+});
+
+//Set up routes for front end
+app.listen(WEB_PORT, () => {
+    console.log(`App Listening on Port: ${WEB_PORT}`); 
+ });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/www/index.html');
+});
+
+app.get('/profile', (req, res) => {
+    res.sendFile(__dirname + '/www/profile.html');
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/www/404.html');
 });
