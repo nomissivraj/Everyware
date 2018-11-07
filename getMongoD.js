96e035c10e1be4ebb0e89f28ddea94604bd210bd
Simon <simonpeterjarvis@gmail.com>
2018-11-07 22:51:37 +0000

    Initial commit | Added function to get last entry from database - using promise to ensure this is finished before page is rendered

---
+++ getMongoD.js
@@ -0,0 +1,34 @@
+//Functions to get data from Mongo
+var exports = module.exports = {};
+
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://testuser:testuser1@ds147440.mlab.com:47440/dat602';
+
+//Function to get last entry with parameters
+exports.getLastEntry = (dbname, collection) => {
+/*
+  Using a promise to resolve the data - otherwise without promise, page render could happen before data is retrieved
+*/
+  return new Promise((resolve, reject) => {
+    //Connect to database
+    MongoClient.connect(url, function(err, db) {
+      //Error handling
+      if (err) throw err;
+      var dbo = db.db(dbname);
+      //Find last document | limit(1) means only get one result | $natural:-1 means reverse order = last document/entry 
+      dbo.collection(collection).find().sort({$natural:-1}).limit(1).toArray( function(err, result) {
+        if (err) throw err;
+        data = result;
+        db.close();
+        console.log(data);
+
+        //evaluate promise - if data exists resolve data otherwise send error
+        if(data) {
+          resolve(data[0]);
+        } else {
+          reject(Error("No data"));
+        }
+      });
+    });
+  });
+};
