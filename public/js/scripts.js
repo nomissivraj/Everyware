eba69e3b05fe515b6bac7abad1f02995df9d0583
Simon <simonpeterjarvis@gmail.com>
2018-11-07 14:39:51 +0000

    initial commit | added menu animation function/event listerners

---
+++ public/js/scripts.js
@@ -0,0 +1,14 @@
+function menuAnimation() {
+    var menu = document.getElementsByTagName('header')[0];
+    var page = document.getElementsByClassName('main')[0];
+    var pageStyles = page.currentStyle || window.getComputedStyle(page);
+    var curLeftMargin = parseInt(pageStyles.marginLeft);
+    console.log(curLeftMargin);
+    
+    menu.addEventListener('mouseover', () => {
+        page.style.marginLeft = curLeftMargin + 117 + "px";
+    });
+    menu.addEventListener('mouseout', () => {
+        page.style.marginLeft = curLeftMargin + "px";
+    });
+}
