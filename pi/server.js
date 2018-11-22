34ed62fa3b3e77261e0f1a9e78c2563cee9f8832
nomissivraj <simonpeterjarvis@gmail.com>
2018-11-22 01:28:10 +0000

    Adding page change functionality based on sensor input - using socket.io

--- pi/server.js
+++ pi/server.js
@@ -1,16 +1,47 @@
 const express = require("express");
 const app = express();
-
+const SerialPort = require("serialport");
 const port = 3000;
+const opn = require('opn');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+server.listen(5000);
+
+io.on("connection", (socket) => {
+	setInterval(() => {
+		socket.emit("bookOpen", state);
+	}, 1000);
+    
+});
+
+const serial = new SerialPort('/dev/ttyUSB0', {	baudRate: 9600});// Monitor pi usb serial port
+var state;// Variable to hold diary state true = open | false = closed
+
+//Serial Monitoring of sensor to check if diary is open/active or not
+//Puts port in flowing mode
+serial.on('data', (data) => {
+    //console.log('Data:', parseInt(data));
+    if (parseInt(data) > 200) {
+        state = true;
+    } else {
+        state = false;
+    }
+});
+
 
 app.use(express.static('www'));
 
 app.get('/', (req, res) =>{
     res.sendFile(__dirname + "/www/visualisation.html");
+    //state == true ? res.redirect('/input') : res.redirect('/');
 });
+
 app.get('/input', (req, res) =>{
     res.sendFile(__dirname + "/www/input.html");
 });
+
 app.listen(port, () => {
     console.log(`App listening on port: ${port}`);
 });
+
+opn('http://localhost:3000');
