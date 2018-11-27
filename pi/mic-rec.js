7b7d1432b37aaa15c8203e0ce455ea1604f56a26
nomissivraj <simonpeterjarvis@gmail.com>
2018-11-27 12:44:19 +0000

    Got the audio file recording/stopping working

--- pi/mic-rec.js
+++ pi/mic-rec.js
@@ -1,6 +1,7 @@
 var exports = module.exports = {};
+const exec = require('child_process').exec;
 
-exports.startRecord = () => {
+exports.startRecord = (dir, file) => {
     console.log("recording");
     // executes command - parameters are error, standard output, standard error
     child = exec("arecord -r 16000 -c 1  -f S16_LE " + dir + file, (error, stdout, stderr) => {
@@ -10,11 +11,9 @@ exports.startRecord = () => {
 };
 
 exports.stopRecord = () => {
-    function stopMic() {
         console.log("Stopped recording");
-        child = exec("pkill arecord", (error, stdout, stderr) => {
+        child2 = exec("pkill arecord", (error, stdout, stderr) => {
             stderr ? console.log('stderr: ' + stderr) : null;
             error !== null ? console.log('exec error: ' + error) : null;
         });
-    }
 };
--- pi/server.js
+++ pi/server.js
@@ -5,10 +5,11 @@ const port = 3000;
 const opn = require('opn');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const Sys = require('util');
-const exec = require('child_process').exec;
+//const Sys = require('util');
+//const exec = require('child_process').exec;
+const { spawn } = require('child_process');
 var child;//child process variable
-const microphone = require('./mic-rec.js');
+//const microphone = require('./mic-rec.js');
 var micState; //Variable to set state of microphone for recording funcitons
 const serial = new SerialPort('/dev/ttyUSB0', {	baudRate: 9600});// Monitor pi usb serial port
 var state;// Variable to hold diary state true = open | false = closed
@@ -17,7 +18,7 @@ var state;// Variable to hold diary state true = open | false = closed
 //Puts port in flowing mode
 serial.on('data', (data) => {
     //console.log('Data:', parseInt(data));
-    if (parseInt(data) > 200) {
+    if (parseInt(data) > 50) {
         state = true;
     } else {
         state = false;
@@ -34,10 +35,14 @@ io.on("connection", (socket) => {
     //Send book status to client every second - where it will be checked and page changed accordingly.
     socket.on('startRecord', (data) => {
         micState = data;
+	handleRecording();
+	console.log("state: " + micState);
     });
 
     socket.on('stopRecord', (data) => {
-        micState = data;
+	micState = data;
+	handleRecording();
+	console.log("state: " + micState);
     });
 
     socket.on('saveEntry', (data) => {
@@ -45,18 +50,50 @@ io.on("connection", (socket) => {
     });
 });
 
-//Switch statement to handle recording based on state of book open/closed/stopped etc.
-switch(micState) {
-    case "record":
-      microphone.startRecord("/home/pi/", "test1.wav"); 
-      break;
-    case "stop":
-      microphone.stopRecord();
-      break;
-    default: 
-      console.log('idle');
-      break;
-  }
+
+function startRecording(dir,file) {
+  console.log("recording");
+	
+	child = spawn('arecord', ['-r 16000', '-c 1', '-fS16_LE', dir+file]);   
+
+	child.stdout.on('data', (data) => {
+	  console.log(`stdout: ${data}`);
+	});
+	
+	child.stderr.on('data', (data) => {
+	  console.log(`stderr: ${data}`);
+	});
+	
+	child.on('close', (code) => {
+	  console.log(`child process exited with code ${code}`);
+	});
+}
+
+function stopRecording() {
+	console.log('killing');
+	child.kill();
+}
+
+
+handleRecording();
+
+function handleRecording() {
+	//Switch statement to handle recording based on state of book open/closed/stopped etc.
+	switch(micState) {
+	    case "record":
+	      //microphone.startRecord("/home/pi/", "test1.wav");
+		startRecording("/home/pi/", "test1.wav"); 
+	      break;
+	    case "stopped":
+		console.log('stopping the recording?');
+	      //microphone.stopRecord();
+		stopRecording();
+	      break;
+	    default: 
+	      console.log('idle');
+	      break;
+	  }
+}
 
 
 //Font end web routes etc. :
@@ -65,7 +102,6 @@ app.use(express.static('www'));
 
 app.get('/', (req, res) =>{
     res.sendFile(__dirname + "/www/visualisation.html");
-    //state == true ? res.redirect('/input') : res.redirect('/');
 });
 
 app.get('/input', (req, res) =>{
