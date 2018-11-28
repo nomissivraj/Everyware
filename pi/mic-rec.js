db2f4869e5fcc1369621f48dce7b0f2714459e5a
nomissivraj <simonpeterjarvis@gmail.com>
2018-11-28 18:20:38 +0000

    updated to spawn child_process rather than exec

--- pi/mic-rec.js
+++ pi/mic-rec.js
@@ -1,19 +1,30 @@
 var exports = module.exports = {};
 const exec = require('child_process').exec;
+const speechText = require('./speech-text');
+var child;
 
-exports.startRecord = (dir, file) => {
+exports.startRecording = (dir, file) => {
     console.log("recording");
-    // executes command - parameters are error, standard output, standard error
-    child = exec("arecord -r 16000 -c 1  -f S16_LE " + dir + file, (error, stdout, stderr) => {
-        stderr ? console.log('stderr: ' + stderr) : null;
-        error !== null ? console.log('exec error: ' + error) : null;
-    });
+    	child = spawn('arecord', ['-r 16000', '-c 1', '-fS16_LE', dir+file]);
+
+	child.stdout.on('data', (data) => {
+	  console.log(`stdout: ${data}`);
+	});
+
+	child.stderr.on('data', (data) => {
+	  console.log(`stderr: ${data}`);
+	});
+
+	child.on('close', (code) => {
+	  console.log(`child process exited with code ${code}`);
+	});
 };
 
-exports.stopRecord = () => {
-        console.log("Stopped recording");
-        child2 = exec("pkill arecord", (error, stdout, stderr) => {
-            stderr ? console.log('stderr: ' + stderr) : null;
-            error !== null ? console.log('exec error: ' + error) : null;
-        });
+exports.stopRecording = () => {
+    console.log("Stopped recording");
+    if (child) child.kill();
+    var text = speechText.toText('/home/pi/', 'test1.wav');
+  //var tempMessage = "To start off, I think I completely failed my geometry quiz, which I know I should’ve studied more for...my dad’s not gonna be happy about that. :( Then, we had a pop quiz in history on the reading homework from last night, and I completely forgot most of what I read, which made me even more upset because I actually did the reading! But what really made me mad was the note that Sarah slipped into my locker during passing period. She said she was sad that I’ve been hanging out with Jane more lately and thinks that I don’t want to be her friend anymore. I can’t believe she thinks that, especially after talking with her on the phone for hours and hours last month while she was going through her breakup with Nick! Just because I’ve been hanging out with Jane a little more than usual doesn’t mean I’m not her friend anymore. She completely blew me off at lunch, and when I told Jane, she thought that Sarah was being a";
+    
+  //beginAnalysis(tempMessage);
 };
