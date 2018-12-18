e0a496e3506c9a65085e18594201b33b57f42085
nomissivraj <simonpeterjarvis@gmail.com>
2018-12-18 15:45:07 +0000

    Updating comments

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const WEB_PORT = 4000; //port for webserver to run on
 //dependencies for general nodejs server routing etc...
 const express = require('express');
 const app = express();
-
-//Front End
-const getMongoD = require("./getMongoD");
+const getMongoD = require("./getMongoD"); // Serverside database calls for profile and history
 const hbs = require('express-handlebars'); // require handlebars
+
+// Seting up handlebars template engine and some defining some helper functions for this
 app.engine('hbs', hbs({
     helpers: {
         equals: (a, b, options) => {
-            return a === b ? options.fn(this) : options.inverse(this);
+            return a === b ? options.fn(this) : options.inverse(this);// Helper function to check for equality between two parameters
+            //options = data contained in #equals block which will be returned/displayed if equality acheieved else will not be displayed
         },
         toLowerCase: (str) => {
             return str.toLowerCase();
+            //simple function to return lowercase (using to set css classes based on variables/names that my be camel cased or capitalised)
         },
     },
     extname: 'hbs',
@@ -20,6 +22,7 @@ app.engine('hbs', hbs({
     layoutsDir: __dirname + '/views/layout/'
 })); //register engine and helpers
 
+//Front End
 app.set('view engine', 'hbs'); // setting view engine to handlebars
 const path = require('path');
 app.use(express.static(path.join(__dirname, '/public')));
