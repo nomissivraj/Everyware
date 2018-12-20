378715e964706816767684049c9e03b89926105f
nomissivraj <simonpeterjarvis@gmail.com>
2018-12-20 23:42:54 +0000

    Reorganised dependncies and comments & added code to record windows mic

--- pi/server.js
+++ pi/server.js
@@ -1,3 +1,4 @@
+// Dependencies
 const express = require("express");
 const app = express();
 const SerialPort = require("serialport");
@@ -7,28 +8,29 @@ const SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
 //const opn = require('opn');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-//const Sys = require('util');
-//const exec = require('child_process').exec;
 const { spawn } = require('child_process');
-//var child;//child process variable
-//const microphone = require('./mic-rec.js');
-var micState; //Variable to set state of microphone for recording funcitons
-const serial = new SerialPort('COM5', {	baudRate: 9600});// Monitor pi usb serial port
-var bookState;// Variable to hold diary state true = open | false = closed
-//const speechText = require('./speech-text.js');
+const serial = new SerialPort('COM3', {	baudRate: 9600})// Monitor pi usb serial port
 
-//function requirements
+// Exported functions
 const mongoDB = require('./push-mongo-DB.js'); //enable access to pushtomongo function
 const personalityInsight = require('./personality-insight.js'); //access to personality insight analysis
 const toneAnalysis = require('./tone-analysis.js'); //access to tone analysis
 
+// Program Variables 
+var micState; //Variable to set state of microphone for recording funcitons
+var bookState; // Variable to hold diary state true = open | false = closed
+var dir = "./audio/"; //Directory to store diary audio entries
+var file = "entry.wav"; //Filename of diary audio entry
+
+
 
 //Serial Monitoring of sensor to check if diary is open/active or not
 //Puts port in flowing mode
+//console.log("serial: ",serial);
 serial.on('data', (data) => {
 	if (parseInt(data) > 0 ) {
 		//console.log('Data:', parseInt(data));
-		if (parseInt(data) > 50) {
+		if (parseInt(data) > 30) {
 			bookState = true;
 		} else {
 			bookState = false;
@@ -38,62 +40,44 @@ serial.on('data', (data) => {
 
 //Sockets (server-side/client-side communication)
 server.listen(5000); // Port for socket to listen on
-
+var sent = 0;
 io.on("connection", (socket) => {
     // EMIT TO CLIENT
     setInterval(() => {
-	socket.emit("bookOpen", bookState);
-	if (transcript) {
-	    socket.emit("transcript", transcript);	
-	    transcript = null;
-	    sent = 0;
-	}
+		socket.emit("bookOpen", bookState);
+		if (transcript) {
+			socket.emit("transcript", transcript);	
+			transcript = null;
+			sent = 0;
+		}
     }, 1000);
-    //Send book status to client every second - where it will be checked and page changed accordingly.
-    
-    //onResolveSpeech(socket);
-        
+    //Send book status to client every second - where it will be checked and page changed accordingly.        
     
     // BACK FORM CLIENT: 
     socket.on('startRecord', (data) => {
         micState = data;
-	console.log("micState from client: " + micState);
-	handleRecording();
+		console.log("micState from client: " + micState);
+		handleRecording();
 	
     });
 
     socket.on('stopRecord', (data) => {
-	micState = data;
-	console.log("micState from client: " + micState);
-	handleRecording();
+		micState = data;
+		console.log("micState from client: " + micState);
+		handleRecording();
     });
 
     socket.on('saveEntry', (data) => {
-	console.log("saving text: ", data);
-	beginAnalysis(data);
+		console.log("saving text: ", data);
+		beginAnalysis(data);
     });
     
     socket.on('newFlower', (data) => {
-	mongoDB.PushtoMongoReplaceNewFlower("ActiveFlower", data);
+		mongoDB.PushtoMongoReplaceNewFlower("ActiveFlower", data);
     });
 
 
 });
-var sent = 0;/*
-
-function onResolveSpeech(socket) {
-    //This function checks every second until 
-    sent = 0;
-	setInterval(() => {
-	    console.log("checking transcript: ", transcript);
-	    if (transcript && sent === 0) {
-		socket.emit("transcript", transcript);
-		sent = 1;
-	    }
-	}, 1000);
-}*/
-
-
 //END SOCKETS
 
 
@@ -102,8 +86,19 @@ var child;
 function startRecording(dir,file) {
   console.log("recording");
 
-  	child = spawn('sox', ['-t waveaudio', '-d', dir+file]);
+	child = spawn('sox', ['-t', 'waveaudio', '-d', dir+file]);
+		child.stdout.on('data', (data) => {
+		console.log(`stdout: ${data}`);
+	});
 
+	child.stderr.on('data', (data) => {
+		console.log(`stderr: ${data}`);
+	});
+
+	child.on('close', (code) => {
+		console.log(`child process exited with code ${code}`);
+	});
+	 
 	/* Following code is for Raspberry Pi/Linux
 	child = spawn('arecord', ['-r 16000', '-c 1', '-fS16_LE', dir+file]);
 
@@ -122,7 +117,7 @@ function startRecording(dir,file) {
 
 function stopRecording() {
     /*if (child)*/ child.kill();
-    toText('./', 'test1.wav');
+    toText(dir, file);
     micState = null;
   //var tempMessage = "To start off, I think I completely failed my geometry quiz, which I know I should’ve studied more for...my dad’s not gonna be happy about that. :( Then, we had a pop quiz in history on the reading homework from last night, and I completely forgot most of what I read, which made me even more upset because I actually did the reading! But what really made me mad was the note that Sarah slipped into my locker during passing period. She said she was sad that I’ve been hanging out with Jane more lately and thinks that I don’t want to be her friend anymore. I can’t believe she thinks that, especially after talking with her on the phone for hours and hours last month while she was going through her breakup with Nick! Just because I’ve been hanging out with Jane a little more than usual doesn’t mean I’m not her friend anymore. She completely blew me off at lunch, and when I told Jane, she thought that Sarah was being a";
 
@@ -201,7 +196,7 @@ function handleRecording() {
 	//Switch statement to handle recording based on state of book open/closed/stopped etc.
 	switch(micState) {
 	    case "record":
-		startRecording(__dirname+"/audio/", "test1.wav");
+		startRecording(dir, file);
 		console.log('current state:', micState);
 	      break;
 	    case "stopped":
