a01279067ee17953b4b138196f22ead073c1c5c1
nomissivraj <simonpeterjarvis@gmail.com>
2018-12-27 18:14:18 +0000

    adding flower script to website

---
+++ public/js/flower.js
@@ -0,0 +1,63 @@
+function Flower(color, petals, currentScore, oldScore, active, parent){
+    this.color = color;
+    this.petals = petals;
+    this.currentScore = currentScore;
+    this.health = currentScore - oldScore;
+    this.state;
+    
+    if(active){
+        if(currentScore <= 20){
+            this.state = "flower0";
+        } else if(this.currentScore <= 50){
+            if(this.health >= 0){
+                this.state = "flower1";
+            } else {
+                this.state = "illflower1";
+            }
+        } else if(this.currentScore <= 70){
+            if(this.health >= 0){
+                this.state = "flower2";
+            } else {
+                this.state = "illflower2";
+            }
+        } else if(this.currentScore < 100){
+            if(this.health >= 0){
+                this.state = "flower3";
+            } else {
+                this.state = "illflower3";
+            }
+        }
+        if(this.currentScore >= 100){
+            this.state = "flower4";
+        }
+        
+        flowerSprite = new Sprite(loader.resources[this.state].texture);
+        flowerSprite.anchor.set(0.5, 1);
+        flowerSprite.position.set(canvWidth * 0.5, canvHeight * 0.84);
+        if(this.currentScore >= 100){
+            
+            //draw the petals and tint
+            let petalString = "petals" + this.petals;
+            
+            let petalSprite = new Sprite(loader.resources[petalString].texture);
+            petalSprite.anchor.set(0.5);
+            petalSprite.tint = this.color;
+            petalSprite.y = -600;
+            flowerSprite.addChild(petalSprite);
+            
+            //create the center of the flower
+            let flowerCenter = new Sprite(loader.resources.flowerCenter.texture);
+            flowerCenter.anchor.set(0.5);
+            flowerCenter.y = -600;
+            flowerSprite.addChild(flowerCenter);
+        }
+        flowerSprite.scale.set(0.8);
+        parent.addChild(flowerSprite);
+        
+        
+        
+    }
+    
+    
+    
+}
