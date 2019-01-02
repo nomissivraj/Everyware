d9f103ac0f9eceed6cc550839f3a7fcf20af36df
nomissivraj <simonpeterjarvis@gmail.com>
2019-01-02 02:35:00 +0000

    added function to parse age from date of birth

---
+++ funcs.js
@@ -0,0 +1,13 @@
+var exports = module.exports = {};
+
+exports.parseAge = (dob) => {
+    var year = Number(dob.substr(0, 4));
+    var month = Number(dob.substr(4, 2)) - 1;
+    var day = Number(dob.substr(6, 2));
+    var today = new Date();
+    var age = today.getFullYear() - year;
+    if (today.getMonth() < month || (today.getMonth() == month && today.getDate() < day)) {
+    age--;
+    }
+    return age;
+}
