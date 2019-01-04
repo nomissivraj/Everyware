9c359e7f42dfb19e39587b6c7cf49f6c7f40a914
nomissivraj <simonpeterjarvis@gmail.com>
2019-01-04 02:31:25 +0000

    Added function to merge collections for better datablock building

--- funcs.js
+++ funcs.js
@@ -11,3 +11,21 @@ exports.parseAge = (dob) => {
     }
     return age;
 }
+
+exports.mergeCollections = (collections) => {
+    var colLengths = [];
+    var mergedCols = [];
+    for (let i = 0; i < collections.length; i++) {
+        colLengths.push(collections[i].length);
+    }
+    var maxColLength = Math.max.apply(null, colLengths);
+    for (let i = 0; i < maxColLength; i++) {
+        var obj = {
+            "personality": collections[0][i],
+            "diaryEntry": collections[1][i],
+            "tone": collections[2][i]
+        }
+        mergedCols.push(obj);
+    }
+    return mergedCols;
+} 
